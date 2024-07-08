c1c35849e0f9ff8f3c25879246a6542acae97241
Cody Johnson <codyjohnsontx@gmail.com>
2024-07-07 22:22:48 -0500

    add tailwindcss and add build commands for netlify

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+// tailwind.config.js
+module.exports = {
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        black: "#000",
+        white: "#fff",
+        gray: {
+          100: "#f5f5f5",
+          200: "#e5e5e5",
+          300: "#d4d4d4",
+          400: "#a3a3a3",
+          500: "#737373",
+          600: "#525252",
+          700: "#404040",
+          800: "#262626",
+          900: "#171717",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
